c13f7f45afd23c204ccbedc234640e94d1e93f3f
Shaik Mujahid <mujahidshaik776@gmail.com>
2026-08-14 10:39:54 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
-alert("Welcome to the NRIIT Learning Management System");
-let header = document.getElementById("welcome");
-header.style.color = "red";
+let registerForm=document.getElementById('registerForm')
+if(registerForm){
+    registerForm.addEventListener("submit",function(event){
+        event.preventDefault();
+        console.log(event);
+        let name=document.getElementById("name").value;
+        let email=document.getElementById("email").value;
+        let password=document.getElementById("password").value;
+        let date=document.getElementById("date").value;
+        let male=document.getElementById("male").checked;
+        let female=document.getElementById("female").checked;
+        let course=document.querySelector('input[name="course"]:checked').value;
+        let gender="";
+        if(male){
+            gender="male";
+        }else if(female){
+            gender="female";
+        }
+        if(name==""||email==""||password==""||date==""||gender==""||course==""){
+            alert("Please fill all the fields");
+        }else{
+            alert("Registration Successful");
+        }
+       }
+      )
+}
+let loginForm=document.getElementById('loginForm')
+if(loginForm){
+    loginForm.addEventListener("submit",function(event){
+        event.preventDefault();
+        console.log(event);
+        let email=document.getElementById("email").value;
+        let password=document.getElementById("password").value;
+        if(email==""||password==""){
+            alert("Please fill all the fields");
+        }else{
+            alert("Login Successful");
+        }
+       }
+      )
+} 
